027b330e6ee6b87aa1de49d72dd86791a83ce52e
KonstantIMP <mihedovkos@gmail.com>
2022-04-25 18:28:59 +0000

    Change ui
    * Remove useless views
    * Add binding init

--- app/src/main/java/org/kimp/witelokk/museum/app/ui/activities/MainActivity.kt
+++ app/src/main/java/org/kimp/witelokk/museum/app/ui/activities/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.kimp.witelokk.museum.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding.root)
     }
 }
