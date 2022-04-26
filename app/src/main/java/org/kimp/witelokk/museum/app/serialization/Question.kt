0a2525973cbb1091bad028fbb0618f986ee5d04a
KonstantIMP <mihedovkos@gmail.com>
2022-04-26 23:11:26 +0000

    Image support

--- app/src/main/java/org/kimp/witelokk/museum/app/serialization/Question.kt
+++ app/src/main/java/org/kimp/witelokk/museum/app/serialization/Question.kt
@@ -2,6 +2,7 @@ package org.kimp.witelokk.museum.app.serialization
 
 data class Question(
     val number: Int,
+    val image: String = "",
     val text: String,
     val type: QuestionType = QuestionType.UNKNOWN,
     val correctAnswer: String,
